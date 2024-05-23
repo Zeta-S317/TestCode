d7a879e6f2715e9d3aea79c66e31a237e37ed2e4
seinm <920650389@qq.com>
2024-05-23 09:50:46 +0800

    更新代码

--- PythonProject/test.py
+++
@@ -1,8 +0,0 @@
-def outtest(info):
-    if info == "hello world":
-        assert(1 > 2)
-    else:
-        print(info)
-
-info = input("请输入内容:")
-outtest(info)
---
+++ PythonProject/test1.py
@@ -0,0 +1,21 @@
+int_number = 10
+float_number = 20.5
+str_text = "hello world"
+
+
+
+list_box = [1,2,["list嵌套", "test"],4,"5"]
+list_box.append("新增内容")
+list_box.pop(2)
+del list_box[1]
+
+_text = [1,2,3]
+
+
+_text.sort(reverse=True)
+
+
+
+
+print(type(list_box))
+print(_text)
