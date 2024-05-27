3b89f92895eb9e019ada0dd33903f7b66846343a
seinm <920650389@qq.com>
2024-05-27 11:10:35 +0800

    了解代码规范

--- PythonProject/train/Python练习代码.py
+++ PythonProject/train/Python练习代码.py
@@ -1,50 +1,29 @@
-int_number = 10
-float_number = 20.5
-str_text = "hello world"
-
-list_box = [1, 2, ["list嵌套", "test"], 4, "5"]
-list_box.append("新增内容")
-list_box.pop(2)
-del list_box[1]
-
-_text = [1, 2, 3] * 5
-a = 10
-b = 10
-
-_text.sort(reverse=True)
-
-TEST = 123
-
-
 class Test:
 
     def __init__(self, username, level):
         self.username = username
         self.level = level
 
-    def logout(self):  # 打印内容
+    def login(self):  # 打印内容
         print(f"你的昵称是{self.username}，等级是{self.level}")
 
 
-targe1 = Test("测试", 100).logout()  # 实例化对象
+Test("测试", 100).login()
 
 
-class stats:
+class Stats:
     id = 0  # 角色Id
     username = ""  # 角色名称
     heath = 100  # 生命值
     speed = 12  # 速度
 
 
-class role1(stats):
+class CustomRoleAdmin(Stats):
 
     def __init__(self):
-        role1.username = "管理员"
-        role1.speed = 20
-        print(f"ID：{role1.id}\nUsername：{role1.username}\nheath：{role1.heath}\nspeed：{role1.speed}")
+        CustomRoleAdmin.username = "管理员"
+        CustomRoleAdmin.speed = 20
+        print(f"====你的角色面板====\nID：{CustomRoleAdmin.id}\nUsername：{CustomRoleAdmin.username}\nheath：{CustomRoleAdmin.heath}\nspeed：{CustomRoleAdmin.speed}")
 
 
-role1()
-
-sum = lambda a, b: a + b
-print(sum(1, 2))
+CustomRoleAdmin()
