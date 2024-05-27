9f21c092c8beab1db435535972cb996f2b23b0be
seinm <920650389@qq.com>
2024-05-27 15:30:34 +0800

    更新代码

--- PythonProject/train/Python函数练习代码.py
+++ PythonProject/train/Python函数练习代码.py
@@ -9,3 +9,14 @@ def change_info(data):
 change_info(list_data)
 
 print(list_data)
+
+
+def print_info(arg1, *test):
+    print(arg1)
+    for var in test:
+        print(var)
+    return
+
+
+# print_info(10)
+print_info(30, 40, 50)
