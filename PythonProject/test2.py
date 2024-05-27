6c35a9971a094790fbfcf7d6dafb61ac6d7e8a89
seinm <920650389@qq.com>
2024-05-27 09:43:29 +0800

    清理了无用的代码

--- PythonProject/test2.py
+++ PythonProject/test2.py
@@ -1,7 +1,7 @@
 import json
-import pickle
 
-# 假设你的JSON数据存储在一个文件中，文件名为 data.json
+
+
 with open('func_data_summary.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
@@ -16,45 +16,42 @@ select_keys = ["name", "total_time_percentage"]
 
 list_box = []
 
-def test(list_data):
+def Get_children(list_data):
     if isinstance(list_data, list):
         for i in list_data:
-            test(i)
+            Get_children(i)
     elif isinstance(list_data, dict):
         if "children" in list_data:
             select_dic = {c_key: list_data[c_key] for c_key in select_keys}
             list_box.append(select_dic)
             for k in list_data["children"]:
-                test(k)
+                Get_children(k)
         else:
             select_dic = {c_key: list_data[c_key] for c_key in select_keys}
             list_box.append(select_dic)
 
+Get_children(data)
 
-test(data)
-# print(list_box)
 
 
 test_dic = []
 time = False
 
-pass
-
 def test2():
-    # 初始化一个空字典用于累加 total_time_percentage
+    # 初始化一个空字典用于累加
     name_dic = {}
 
-    # 遍历 list_box，累加 total_time_percentage
+    # 遍历 list_box，然后存放
     for i in list_box:
-        name = i["name"]
-        total_time_percentage = i["total_time_percentage"]
+        v1 = i["name"]
+        v2 = i["total_time_percentage"]
 
-        if name not in name_dic:
-            name_dic[name] = 0
-        name_dic[name] += total_time_percentage
+        if v1 not in name_dic:
+            name_dic[v1] = 0
+        name_dic[v1] += v2
 
     # 将累加结果转换为所需的列表形式
-    result_list = [{"name": name, "total_time_percentage": total_time_percentage} for name, total_time_percentage in
+    result_list = [{"name": v1, "total_time_percentage": v2} for v1, v2 in
                    name_dic.items()]
 
     # print(result_list)
@@ -66,11 +63,9 @@ test2()
 
 
 list_box2 = []
-storage = []
 
 #
 def test3(list_data, path=""):
-    count = 0
     if isinstance(list_data, list):
         for i in list_data:
             test3(i, path)
@@ -84,7 +79,6 @@ def test3(list_data, path=""):
             select_dic = {c_key: list_data[c_key] for c_key in select_keys}
             select_dic["name"] = full_path
             list_box2.append(select_dic)
-            count += 1
             for k in list_data["children"]:
                 test3(k, full_path)
         else:
@@ -93,15 +87,8 @@ def test3(list_data, path=""):
             select_dic["name"] = current_path
             list_box2.append(select_dic)
 
-# 调用函数示例
 test3(data)
 
 print(list_box2)
 
-#
-#
-#
-#
-# test3(data)
-# print(list_box2)
 
