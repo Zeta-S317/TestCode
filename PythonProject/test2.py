62ff5c306fb17ecd668ca54d041fe450b72d7971
seinm <920650389@qq.com>
2024-05-27 10:34:42 +0800

    优化了代码

--- PythonProject/test2.py
+++ PythonProject/test2.py
@@ -1,94 +1,90 @@
 import json
 
-
-
 with open('func_data_summary.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
+    json_data = json.load(f)
 
-a = [{"self_time": 2220.8094799999994, "name": "PlayerLoop", "total_time": 298504.70992,
-      "avg_total_time": 99.10514937583001, "calls": 3012, "gc_mem": 66502.0986328125
-         , "gt_10ms": 3012, "max_total_time": 5370.848, "calls_pre_frame": 1.0, "total_time_percentage": 100.0}]
+# 第一题，获取全部为name和total_time_percentage的数据，包括children里的
 
-# select_keys = ["self_time", "name", "total_time", "avg_total_time", "calls", "gc_mem",
-#        "gt_10ms", "max_total_time", "calls_pre_frame", "total_time_percentage"]
+all_data = []
 
-select_keys = ["name", "total_time_percentage"]
 
-list_box = []
-
-def Get_children(list_data):
-    if isinstance(list_data, list):
-        for i in list_data:
-            Get_children(i)
-    elif isinstance(list_data, dict):
-        if "children" in list_data:
-            select_dic = {c_key: list_data[c_key] for c_key in select_keys}
-            list_box.append(select_dic)
-            for k in list_data["children"]:
-                Get_children(k)
+def get_children(data):
+    select_keys = ["name", "total_time_percentage"]
+    if isinstance(data, list):
+        for i in data:
+            get_children(i)
+    elif isinstance(data, dict):
+        if "children" in data:
+            select_dic = {c_key: data[c_key] for c_key in select_keys}
+            all_data.append(select_dic)
+            for k in data["children"]:
+                get_children(k)
         else:
-            select_dic = {c_key: list_data[c_key] for c_key in select_keys}
-            list_box.append(select_dic)
-
-Get_children(data)
+            select_dic = {c_key: data[c_key] for c_key in select_keys}
+            all_data.append(select_dic)
 
 
+get_children(json_data)
 
-test_dic = []
-time = False
+# 控制台输出所有数据，包括children下的,但不包括路径
+# print(all_data)
+# 控制台输出所有数据，包括children下的,但不包括路径
 
-def test2():
-    # 初始化一个空字典用于累加
+# 第二题 提取里面的name的值和total_time，同样name的值合并，并且total_time相加
+
+name_list = []  # 合并相同name的值，并且它们的total_time相加
+
+
+def get_dic_values():
     name_dic = {}
 
-    # 遍历 list_box，然后存放
-    for i in list_box:
-        v1 = i["name"]
-        v2 = i["total_time_percentage"]
+    for i in all_data:
+        name_value = i["name"]
+        number_value = i["total_time_percentage"]
 
-        if v1 not in name_dic:
-            name_dic[v1] = 0
-        name_dic[v1] += v2
+        if name_value not in name_dic:
+            name_dic[name_value] = 0
+        name_dic[name_value] += number_value
 
-    # 将累加结果转换为所需的列表形式
-    result_list = [{"name": v1, "total_time_percentage": v2} for v1, v2 in
-                   name_dic.items()]
-
-    # print(result_list)
+    name_list.append([{"name": v1, "total_time_percentage": v2} for v1, v2 in name_dic.items()])
 
 
-test2()
+get_dic_values()
+
+# 控制台输出提取后的值
+# print(name_list)
+# 控制台输出提取后的值
 
 
+# 第三题，带有全部路径的为name和total_time_percentage所有数据
+all_data_path = []
 
-
-list_box2 = []
 
 #
-def test3(list_data, path=""):
-    if isinstance(list_data, list):
-        for i in list_data:
-            test3(i, path)
-    elif isinstance(list_data, dict):
-        # 构建当前路径
-        current_path = path + list_data["name"]
+def get_all_path(data, path=""):
+    select_keys = ["name", "total_time_percentage"]
+    if isinstance(data, list):
+        for i in data:
+            print(path)
+            get_all_path(i, path)
+    elif isinstance(data, dict):
+        current_path = path + data["name"]
 
-        if "children" in list_data:
-            # 有子节点时，路径后面添加斜杠
+        if "children" in data:
             full_path = current_path + "/"
-            select_dic = {c_key: list_data[c_key] for c_key in select_keys}
+            select_dic = {c_key: data[c_key] for c_key in select_keys}
             select_dic["name"] = full_path
-            list_box2.append(select_dic)
-            for k in list_data["children"]:
-                test3(k, full_path)
+            all_data_path.append(select_dic)
+            for k in data["children"]:
+                get_all_path(k, full_path)
         else:
-            # 没有子节点时，不添加斜杠
-            select_dic = {c_key: list_data[c_key] for c_key in select_keys}
+            select_dic = {c_key: data[c_key] for c_key in select_keys}
             select_dic["name"] = current_path
-            list_box2.append(select_dic)
-
-test3(data)
-
-print(list_box2)
+            all_data_path.append(select_dic)
 
 
+get_all_path(json_data)
+
+# 控制台输出带路径的全部为name和total_time_percentage数据
+# print(all_data_path)
+# 控制台输出带路径的全部为name和total_time_percentage数据
